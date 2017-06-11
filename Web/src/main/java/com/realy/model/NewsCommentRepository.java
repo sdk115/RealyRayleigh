15fdb8802fad760d59808a611c119155b46def87
성대경 <sdk115@naver.com>
2017-06-12 05:01:49 +0900

    rest api 수정

--- Web/src/main/java/com/realy/model/NewsCommentRepository.java
+++ Web/src/main/java/com/realy/model/NewsCommentRepository.java
@@ -2,12 +2,17 @@ package com.realy.model;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "comment", path = "comment")
 public interface NewsCommentRepository extends JpaRepository<NewsComment, Long>{
 	List<NewsComment> findByKeywordId(int id);
 	List<NewsComment> findByKeywordIdAndCategoryId(int kid, int cid);
 	List<NewsComment> findByKeywordIdAndCategoryIdOrderByRegTimeDesc(int kid, int cid);
+	Page<NewsComment> findByKeywordIdAndCategoryId(Pageable pageable, int kid, int cid);	
 	int countByKeywordId(int id);
 	
 }
--- Web/src/main/java/com/realy/model/NewsKeywordRepository.java
+++ Web/src/main/java/com/realy/model/NewsKeywordRepository.java
@@ -3,8 +3,9 @@ package com.realy.model;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource(collectionResourceRel = "keyword", path = "keyword")
 public interface NewsKeywordRepository extends JpaRepository<NewsKeyword, Long> {
 	NewsKeyword findById(int id);
 }
